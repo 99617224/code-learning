29214f652cf615ea8afe05dafd072407525bedba
99617224 <52458005+99617224@users.noreply.github.com>
2020-04-27 16:04:56 +0800

    1

--- 爬取新闻/爬tweets的突发新闻.py
+++ 爬取新闻/爬tweets的突发新闻.py
@@ -1,21 +1,38 @@
+
 import twint
 import time
 import datetime
-tweets=[]
-newspeople=[]
-newsword=[]
-fiveminutesAgo = (datetime.datetime.now() - datetime.timedelta(minutes=10))
-c=twint.Config()
-c.Search='#BREAKING'
-c.Count='True'
-c.Lang='en'
-c.Store_object='True'
-c.Since=fiveminutesAgo.strftime("%Y-%m-%d %H:%M:%S")
-c.Store_object_tweets_list=tweets
-c.Limit='1'
+import telegram
+
+bot = telegram.Bot(token='957820157:AAG2vjz1gyhrxHc07fF_orRwegX_0I1XOvc')
+tweets = []
+newspeople = []
+newsword = ['china','virus']
+alreadylist = []
+fiveminutesAgo = (datetime.datetime.now() - datetime.timedelta(minutes=5))
+c = twint.Config()
+c.Search = ['#BREAKING']
+c.Count = 'True'
+c.Lang = 'en'
+c.Store_object = 'True'
+c.Since = fiveminutesAgo.strftime("%Y-%m-%d %H:%M:%S")
+c.Store_object_tweets_list = tweets
+c.Limit = '1'
 # c.Translate='True'
 # c.TranslateDest='zh'
-twint.run.Search(c)
+while True:
+    twint.run.Search(c)
 
-for i in tweets:
-    print(i.tweet)
+    for i in tweets:
+        for w in newsword:
+            if w in i.tweet and i.tweet not in alreadylist:
+                print('{0}在{1}{2}发送:{3}.地点：{4}'.format(i.username, str(i.datestamp), 
+                str(i.timestamp), i.tweet, i.place))
+                bot.send_message(chat_id='-488903601', text='{0}在{1}({2})发送:{3}.地点：{4}'.format(
+                i.username, str(i.datestamp), str(i.timestamp), i.tweet, i.place))
+                alreadylist.append(i.tweet)
+                time.sleep(5)
+            else:
+                pass
+    # print(alreadylist)
+    time.sleep(30)
---
+++ 爬取新闻/调用telegram发送信息.py
@@ -0,0 +1,3 @@
+import telegram
+bot = telegram.Bot(token='957820157:AAG2vjz1gyhrxHc07fF_orRwegX_0I1XOvc')
+bot.send_message(chat_id='-488903601', text="hi")
